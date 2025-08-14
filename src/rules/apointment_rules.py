2fcd187167dd7540be0082d7f962ebc7a7ee2207
ustsl <viacheslav.ustinov.dev@gmail.com>
2025-08-14 11:59:15 +0300

    upd

--- src/rules/apointment_rules.py
+++ src/rules/apointment_rules.py
@@ -46,15 +46,15 @@ rules = [
             "processor": napopravku_parse_email,
         },
     },
-    # {
-    #     "rule": {"sender": "info@medflex.ru", "subject": "запись"},
-    #     "action": {
-    #         "type": CRM_QUERY_TYPE,
-    #         "url": CRM_URL,
-    #         "headers": crm_headers,
-    #         "processor": prodoctorov_parse_email,
-    #     },
-    # },
+    {
+        "rule": {"sender": "info@medflex.ru", "subject": "запись"},
+        "action": {
+            "type": CRM_QUERY_TYPE,
+            "url": CRM_URL,
+            "headers": crm_headers,
+            "processor": prodoctorov_parse_email,
+        },
+    },
     {
         "rule": {"sender": "clinic-online@sberhealth.ru", "subject": ""},
         "action": {
